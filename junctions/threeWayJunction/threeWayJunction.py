5cfbe957334931cfd82e17cf3b5a8b2422fe6443
jawad <jawadefaj006@gmail.com>
2021-04-22 21:31:46 -0700

    file created three way junction

---
+++ junctions/threeWayJunction/threeWayJunction.py
@@ -0,0 +1,10 @@
+"""
+PsudoCode
+    create straight road
+    create a random road that is not directly 180
+    if newRoad is in 0 - 180 (ex: 120)
+        remaining angle 60
+    // do we need special treatment for 180 - 360?
+        dont think we need it because the road is just inverse 
+
+"""
