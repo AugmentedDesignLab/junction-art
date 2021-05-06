5697a4e8c26144c8ceac999353eb4c3218a97f42
Adhocmaster <adhocmaster@live.com>
2021-05-06 00:56:14 -0700

    outline for adapter written

--- roadgen/library/IntersectionAdapter.py
+++ roadgen/library/IntersectionAdapter.py
@@ -1,3 +1,13 @@
 class IntersectionAdapter:
 
-    pass
+    def odrTo4DirectionIntersection(self, odr):
+
+        #1. TODO if not adjusted, adjust
+
+        #2. find the minimum bounding box
+
+        #3. find the center of the bounding box
+
+        #4. assign direction based on the position of the incident points
+
+        #5. adjust direction based on the heading.
