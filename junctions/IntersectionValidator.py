rotation yields wrong direction intersections.

junctions/IntersectionValidator.py
@@ -44,6 +44,7 @@ class IntersectionValidator:
             distance = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
             if distance < minConnectionLength:
+                print("invalid distance")
                 return False
 
         return True
@@ -59,9 +60,10 @@ class IntersectionValidator:
         firstRoad = intersection.incidentRoads[0]
         x, y, h = firstRoad.getPosition(intersection.incidentCPs[0])
 
-        h = math.ceil((h + np.pi * 2) % np.pi)
-        if h != 3 and h != 0:
-            raise Exception(f"{self.name}: validateDirectionAndPositionAgreement: cannot validated rotated intersection h = {h}")
+        h = h % (np.pi * 2)
+        # h = math.ceil((h + np.pi * 2) % np.pi)
+        # if h != 3 and h != 0:
+        #     raise Exception(f"{self.name}: validateDirectionAndPositionAgreement: cannot validated rotated intersection h = {h}")
         
 
         # get the last incident roads position and heading
@@ -79,11 +81,23 @@ class IntersectionValidator:
             borderDistanceLast = lastIncidentRoad.getBorderDistanceLeft(lastIP)
 
         if (lastX - borderDistanceLast) < x:
+            print("invalid X pos")
             return False
 
+
+        borderDistanceFirst = firstRoad.getBorderDistanceRight(intersection.incidentCPs[0])
+        if intersection.incidentCPs[0] == pyodrx.ContactPoint.end:
+            borderDistanceFirst = lastIncidentRoad.getBorderDistanceLeft(intersection.incidentCPs[0])
+
         # validate Y
-        if lastH > np.pi and lastH < ((4/3) * np.pi):
-            return lastY < y
+        minH = h + np.pi
+        maxH = (h + (3/2) * np.pi)
+        if lastH > minH and lastH < maxH:
+            if lastY >= (y - borderDistanceFirst):
+                print("invalid Y pos")
+                return False
+        
+        return True
         
 
 

junctions/SequentialJunctionBuilder.py
@@ -188,7 +188,13 @@ class SequentialJunctionBuilder(JunctionBuilder):
         if self.minAngle >= availableAngle:
             return availableAngle
 
-        angle = np.random.uniform(self.minAngle, availableAngle)
+        maxAngle = (maxAnglePerConnection + availableAngle) / 2
+        if maxAngle > availableAngle:
+            maxAngle = availableAngle
+
+        angle = np.random.uniform(self.minAngle, maxAnglePerConnection * 1.3)
+        # angle = np.random.uniform(self.minAngle, maxAngle)
+        # angle = np.random.uniform(self.minAngle, availableAngle)
         # angle = (availableAngle * np.random.choice(10)) / 9
 
         

roadgen/HDMapBuilder.py
@@ -22,6 +22,7 @@ class HDMapBuilder:
         self.p = p # probability distribution of 3-way, 4, 5, 6
         self.intersections = {} # DirectionIntersection -> intersection
         self.placedIntersections = {}
+        self.rotation = {}
         self.nextIntersectionId = startId
         
         self.seed = seed
@@ -30,8 +31,8 @@ class HDMapBuilder:
                                                     maxAngle=np.pi * .75,
                                                     straightRoadLen=5, 
                                                     probLongConnection=0.3,
-                                                    probMinAngle=0.3,
-                                                    probRestrictedLane=0.2,
+                                                    probMinAngle=0.1,
+                                                    probRestrictedLane=0,
                                                     maxConnectionLength=30,
                                                     minConnectionLength=12,
                                                     random_seed=self.seed)
@@ -55,10 +56,12 @@ class HDMapBuilder:
 
     def createIntersections(self):
 
+        print(f"{self.name}: createIntersections")
         minLanePerSide = 1
         maxLanePerSide = 2
         self.nextRoadId = 0
         for sl in range(self.nIntersections):
+            print(f"{self.name}: creating {sl + 1}")
             maxNumberOfRoadsPerJunction = np.random.choice([3, 4, 5])
             intersection = self.createValidIntersection(id, self.nextRoadId, maxNumberOfRoadsPerJunction, minLanePerSide, maxLanePerSide)
             
@@ -67,7 +70,7 @@ class HDMapBuilder:
             self.intersections[directionIntersection] = intersection
 
     
-    def createValidIntersection(self, id, firstRoadId, maxNumberOfRoadsPerJunction, minLanePerSide, maxLanePerSide):
+    def createValidIntersection(self, id, firstRoadId, maxNumberOfRoadsPerJunction, minLanePerSide, maxLanePerSide, rotate=False):
         intersection = self.builder.createWithRandomLaneConfigurations("", 
                             id, 
                             firstRoadId=firstRoadId,
@@ -82,6 +85,9 @@ class HDMapBuilder:
         while (self.validator.validateIncidentPoints(intersection, self.builder.minConnectionLength) == False):
             intersection = self.createValidIntersection(id, self.nextRoadId, maxNumberOfRoadsPerJunction, minLanePerSide, maxLanePerSide)
 
+        if rotate:
+            self.rotation[intersection] = np.random.uniform(0, np.pi * 2)
+            ODRHelper.transform(intersection.odr, startX=0, startY=0, heading=self.rotation[intersection])
         return intersection
 
 
@@ -137,7 +143,12 @@ class HDMapBuilder:
                 intersection = self.intersections[directionIntersection]
                 if self.debug:
                     logging.info(f"Transforming {intersection.id} to ({x}, {y})")
-                ODRHelper.transform(intersection.odr, startX=x, startY=y)
+
+                rotation = 0
+                if intersection in self.rotation:
+                    rotation = self.rotation[intersection]
+
+                ODRHelper.transform(intersection.odr, startX=x, startY=y, heading=rotation)
                 self.placedIntersections[directionIntersection] = intersection
 
                 odrList.append(intersection.odr)

tests/test_SequentialJunctionBuilder.py
@@ -22,7 +22,7 @@ class test_SequentialJunctionBuilder(unittest.TestCase):
                                                     maxAngle=np.pi * .75,
                                                     straightRoadLen=5, 
                                                     probLongConnection=0.5,
-                                                    probMinAngle=0.5,
+                                                    probMinAngle=0.1,
                                                     probRestrictedLane=0.2,
                                                     maxConnectionLength=30,
                                                     minConnectionLength=12,
@@ -404,7 +404,6 @@ class test_SequentialJunctionBuilder(unittest.TestCase):
                 plt.title("Valid")
             plt.show()
             extensions.saveRoadImageFromFile(xmlPath, self.configuration.get("esminipath"))
-            extensions.printRoadPositions(odr)
 
     def test_6WayJunctions(self):
 
@@ -414,20 +413,31 @@ class test_SequentialJunctionBuilder(unittest.TestCase):
         
         for sl in range(5):
             path = self.configuration.get("harvested_straight_roads")
-            odr = self.builder.createWithRandomLaneConfigurations(path, 
-                                0, 
+            intersection = self.builder.createWithRandomLaneConfigurations(path, 
+                                sl, 
                                 maxNumberOfRoadsPerJunction=maxNumberOfRoadsPerJunction, 
                                 maxLanePerSide=maxLanePerSide, 
                                 minLanePerSide=minLanePerSide, 
                                 internalConnections=True, 
                                 cp1=pyodrx.ContactPoint.end,
-                                internalLinkStrategy = LaneConfigurationStrategies.SPLIT_ANY)
+                                internalLinkStrategy = LaneConfigurationStrategies.SPLIT_ANY,
+                                getAsOdr=False)
 
+
+            odr = intersection.odr
             # xmlPath = f"output/test_createWithRandomLaneConfigurations-split-any-{maxNumberOfRoadsPerJunction}-{sl}.xodr"
             xmlPath = f"output/seed-{self.seed}-{maxNumberOfRoadsPerJunction}-way-{sl}.xodr"
             odr.write_xml(xmlPath)
-            
-            # extensions.view_road(odr,os.path.join('..',self.configuration.get("esminipath")))
+            extensions.printRoadPositions(odr)
+            isValid = self.validator.validateIncidentPoints(intersection, self.builder.minConnectionLength)
+            # if isValid == False:
+            #     print(f"{sl} is an invalid intersection")
+            plt = extensions.view_road(odr,os.path.join('..',self.configuration.get("esminipath")), returnPlt=True)
+            if isValid == False:
+                plt.title(f"Invalid - {xmlPath}")
+            else:
+                plt.title(f"Valid - {xmlPath}")
+            plt.show()
             extensions.saveRoadImageFromFile(xmlPath, self.configuration.get("esminipath"))
 
 