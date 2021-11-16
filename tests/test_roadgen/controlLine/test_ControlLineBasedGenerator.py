fb5605e56a57353574ddfe07f8d66d1614fe4973
Adhocmaster <adhocmaster@live.com>
2021-11-16 09:02:41 -0800

    more test codes for performance analysis

--- tests/test_roadgen/controlLine/test_ControlLineBasedGenerator.py
+++ tests/test_roadgen/controlLine/test_ControlLineBasedGenerator.py
@@ -8,8 +8,13 @@ from junctionart.roadgen.controlLine.ControlLineBasedGenerator import ControlLin
 import junctionart.extensions as extensions, os, logging
 import numpy as np
 import traceback
+import time
+from tabulate import tabulate
+
+import psutil
+
 logfile = 'ControlLineBasedGenerator.log'
-logging.basicConfig(level=logging.INFO, filename=logfile)
+logging.basicConfig(level=logging.WARNING, filename=logfile)
 
 
 class test_ControlLineBasedGenerator(unittest.TestCase):
@@ -144,3 +149,191 @@ class test_ControlLineBasedGenerator(unittest.TestCase):
             # xmlPath = f"output/test_generateWithManualControlLines2.xodr"
             # odr.write_xml(xmlPath)
             extensions.view_road(odr, os.path.join('..',self.configuration.get("esminipath"))) 
+
+
+    def test_running_time(self):
+
+        heading = ["test configuration", "#tries", "#success", "total time (minutes)", "time per map (seconds)", "memory (MB)"]
+        stats = []
+
+        count = 50
+        mapsize=(500, 500)
+        # goal generate count 400x400 maps with 2 lanes
+        tryCount, successCount, durationInMin, averageInSec, memory = self.getRunningTimeStatsFor1000Maps(mapsize=mapsize, randomizeLanes=False, nControlLine=3, count=count)
+        # print(f"tries: {tryCount}, success: {successCount}, time: {durationInMin} Seconds, averageInSec: {averageInSec}")
+        stats.append([f"{count} {mapsize} square meter maps with 2 lanes", tryCount, successCount, durationInMin, averageInSec, memory])
+
+        # goal generate count 400x400 maps with random lanes
+        tryCount, successCount, durationInMin, averageInSec, memory = self.getRunningTimeStatsFor1000Maps(mapsize=mapsize, randomizeLanes=True, nControlLine=3, count=count)
+        stats.append([f"{count} {mapsize} square meter maps with random lanes", tryCount, successCount, durationInMin, averageInSec, memory])
+
+        count = 50
+        mapsize=(1000, 1000)
+        # 2 lanes
+        tryCount, successCount, durationInMin, averageInSec, memory= self.getRunningTimeStatsFor1000Maps(mapsize=mapsize, randomizeLanes=False, nControlLine=6, count=count)
+        stats.append([f"{count} {mapsize} square meter maps with 2 lanes", tryCount, successCount, durationInMin, averageInSec, memory])
+
+        # random lanes
+        tryCount, successCount, durationInMin, averageInSec, memory= self.getRunningTimeStatsFor1000Maps(mapsize=mapsize, randomizeLanes=True, nControlLine=6, count=count)
+        stats.append([f"{count} {mapsize} square meter maps with random lanes", tryCount, successCount, durationInMin, averageInSec, memory])
+
+        count = 25
+        mapsize=(5000, 5000)
+        # 2 lanes
+        tryCount, successCount, durationInMin, averageInSec, memory= self.getRunningTimeStatsFor1000Maps(mapsize=mapsize, randomizeLanes=False, nControlLine=25, count=count)
+        stats.append([f"{count} {mapsize} square meter maps with 2 lanes", tryCount, successCount, durationInMin, averageInSec, memory])
+
+        # random lanes
+        tryCount, successCount, durationInMin, averageInSec, memory= self.getRunningTimeStatsFor1000Maps(mapsize=mapsize, randomizeLanes=True, nControlLine=25, count=count)
+        stats.append([f"{count} {mapsize} square meter maps with random lanes", tryCount, successCount, durationInMin, averageInSec, memory])
+
+        # count = 4
+        # mapsize=(10000, 10000)
+        # # 2 lanes
+        # tryCount, successCount, durationInMin, averageInSec, memory= self.getRunningTimeStatsFor1000Maps(mapsize=mapsize, randomizeLanes=False, nControlLine=50, count=count)
+        # stats.append([f"{count} {mapsize} square meter maps with 2 lanes", tryCount, successCount, durationInMin, averageInSec, memory])
+
+        # # random lanes
+        # tryCount, successCount, durationInMin, averageInSec, memory= self.getRunningTimeStatsFor1000Maps(mapsize=mapsize, randomizeLanes=True, nControlLine=50, count=count)
+        # stats.append([f"{count} {mapsize} square meter maps with random lanes", tryCount, successCount, durationInMin, averageInSec, memory])
+
+        # test with manual
+        count = 50
+        mapsize=(1200, 1400)
+        # goal generate count 400x400 maps with 2 lanes
+        tryCount, successCount, durationInMin, averageInSec, memory = self.getRunningTimeStatsFor1000ManualMaps(mapsize=mapsize, randomizeLanes=False, nControlLine=3, count=count)
+        # print(f"tries: {tryCount}, success: {successCount}, time: {durationInMin} Seconds, averageInSec: {averageInSec}")
+        stats.append([f"{count} {mapsize} manual control-line maps with 2 lanes", tryCount, successCount, durationInMin, averageInSec, memory])
+
+        # goal generate count 400x400 maps with random lanes
+        tryCount, successCount, durationInMin, averageInSec, memory = self.getRunningTimeStatsFor1000ManualMaps(mapsize=mapsize, randomizeLanes=True, nControlLine=3, count=count)
+        stats.append([f"{count} {mapsize} manual control-line maps with random lanes", tryCount, successCount, durationInMin, averageInSec, memory])
+
+        # test with A
+        count = 50
+        mapsize=(1000, 1600)
+        # goal generate count 400x400 maps with 2 lanes
+        tryCount, successCount, durationInMin, averageInSec, memory = self.getRunningTimeStatsFor1000AMaps(mapsize=mapsize, randomizeLanes=False, nControlLine=3, count=count)
+        # print(f"tries: {tryCount}, success: {successCount}, time: {durationInMin} Seconds, averageInSec: {averageInSec}")
+        stats.append([f"{count} {mapsize} A-shaped maps with 2 lanes", tryCount, successCount, durationInMin, averageInSec, memory])
+
+        # goal generate count 400x400 maps with random lanes
+        tryCount, successCount, durationInMin, averageInSec, memory = self.getRunningTimeStatsFor1000AMaps(mapsize=mapsize, randomizeLanes=True, nControlLine=3, count=count)
+        stats.append([f"{count} {mapsize} A-shaped maps with random lanes", tryCount, successCount, durationInMin, averageInSec, memory])
+
+
+        print("\n")
+        print(tabulate(stats, headers=heading))
+        print("\n")
+
+
+        pass
+
+    
+
+    def getRunningTimeStatsFor1000Maps(self, mapsize, randomizeLanes, nControlLine, count = 100):
+
+        tryCount = 0
+        successCount = 0
+        startNS = time.process_time_ns()
+        for seed in range(1, count * 2):
+            tryCount += 1
+            try:
+                generator = ControlLineBasedGenerator(mapsize, debug=False, seed=seed, randomizeLanes=randomizeLanes, randomizeDistance=False, randomizeHeading=False)
+                odr = generator.generateWithHorizontalControlines("test_generateWithHorizontalControlines", nControlLine, plotGrid=False)
+
+                # xmlPath = f"output/test_generateWithHorizontalControlines.xodr"
+                # odr.write_xml(xmlPath)
+                # extensions.view_road(odr, os.path.join('..',self.configuration.get("esminipath"))) 
+                successCount += 1
+                if successCount == count:
+                    break
+            except:
+                pass
+
+        endNS = time.process_time_ns()
+
+        # memory stats
+        process = psutil.Process(os.getpid())
+        memory = round(process.memory_info().rss / (1024 * 1024), 2) # MB
+        # extensions.view_road(odr, os.path.join('..',self.configuration.get("esminipath"))) 
+
+        # durationInSec = round((endNS - startNS) / 1_000_000_000, 2)
+        durationInMin = round((endNS - startNS) / 60_000_000_000, 2)
+        averageInSec = round((endNS - startNS)  / (successCount * 1_000_000_000), 2)
+
+        # print(f"memory1 : {memory1}")
+
+        return tryCount, successCount, durationInMin, averageInSec, memory
+
+
+    def getRunningTimeStatsFor1000ManualMaps(self, mapsize, randomizeLanes, nControlLine, count = 100):
+
+        tryCount = 0
+        successCount = 0
+        startNS = time.process_time_ns()
+        for seed in range(1, count * 2):
+            tryCount += 1
+            try:
+                generator = ControlLineBasedGenerator(mapsize, debug=False, seed=seed, randomizeLanes=randomizeLanes, randomizeDistance=False, randomizeHeading=False)
+                odr = generator.generateWithManualControlines("test_generateWithHorizontalControlines", plotGrid = False)
+
+                # xmlPath = f"output/test_generateWithHorizontalControlines.xodr"
+                # odr.write_xml(xmlPath)
+                # extensions.view_road(odr, os.path.join('..',self.configuration.get("esminipath"))) 
+                successCount += 1
+                if successCount == count:
+                    break
+            except:
+                pass
+
+        endNS = time.process_time_ns()
+
+        # memory stats
+        process = psutil.Process(os.getpid())
+        memory = round(process.memory_info().rss / (1024 * 1024), 2) # MB
+        # extensions.view_road(odr, os.path.join('..',self.configuration.get("esminipath"))) 
+
+        # durationInSec = round((endNS - startNS) / 1_000_000_000, 2)
+        durationInMin = round((endNS - startNS) / 60_000_000_000, 2)
+        averageInSec = round((endNS - startNS)  / (successCount * 1_000_000_000), 2)
+
+        # print(f"memory1 : {memory1}")
+
+        return tryCount, successCount, durationInMin, averageInSec, memory
+
+        
+    def getRunningTimeStatsFor1000AMaps(self, mapsize, randomizeLanes, nControlLine, count = 100):
+
+        tryCount = 0
+        successCount = 0
+        startNS = time.process_time_ns()
+        for seed in range(1, count * 2):
+            tryCount += 1
+            try:
+                generator = ControlLineBasedGenerator(mapsize, debug=False, seed=seed, randomizeLanes=randomizeLanes, randomizeDistance=False, randomizeHeading=False)
+                odr = generator.generateWithManualControlines("test_mapA", layout='A', plotGrid=False)
+
+                # xmlPath = f"output/test_generateWithHorizontalControlines.xodr"
+                # odr.write_xml(xmlPath)
+                # extensions.view_road(odr, os.path.join('..',self.configuration.get("esminipath"))) 
+                successCount += 1
+                if successCount == count:
+                    break
+            except:
+                pass
+
+        endNS = time.process_time_ns()
+
+        # memory stats
+        process = psutil.Process(os.getpid())
+        memory = round(process.memory_info().rss / (1024 * 1024), 2) # MB
+        # extensions.view_road(odr, os.path.join('..',self.configuration.get("esminipath"))) 
+
+        # durationInSec = round((endNS - startNS) / 1_000_000_000, 2)
+        durationInMin = round((endNS - startNS) / 60_000_000_000, 2)
+        averageInSec = round((endNS - startNS)  / (successCount * 1_000_000_000), 2)
+
+        # print(f"memory1 : {memory1}")
+
+        return tryCount, successCount, durationInMin, averageInSec, memory
